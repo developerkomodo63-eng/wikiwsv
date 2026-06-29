b19ad5096b3cf96b0d24863a67b0c488b20c78cb
unknown <developerkomodo63@gmail.com>
2026-06-29 14:43:10 -0400

    cuarta version

--- src/MarkdownPage.jsx
+++ src/MarkdownPage.jsx
@@ -10,20 +10,8 @@ function MarkdownPage({ file }) {
   const content = markdownModules[filePath] ?? ''
 
   const formattedContent = content
-    .replace(/!\[[^\]]*\]\((img_jerale\/[^)]+)\)/g, '![](/docs_jerale/$1)')
-    .replace(/!\[[^\]]*\]\(([^)]+)\)/g, (match, p1) => {
-      if (p1.startsWith('/') || p1.startsWith('http') || p1.includes('://')) {
-        return match
-      }
-
-      const normalized = p1.replace(/^img_jerale\//, '')
-      const isImage = /\.(png|jpg|jpeg|gif|webp|svg)$/i.test(normalized)
-
-      if (!isImage) {
-        return match
-      }
-
-      return `![](/docs_jerale/img_jerale/${normalized})`
+    .replace(/!\[([^\]]*)\]\(img_jerale\/([^)]+)\)/g, (match, alt, filename) => {
+      return `![${alt}](/docs_jerale/img_jerale/${filename})`
     })
 
   return (
